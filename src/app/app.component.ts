0f86d2bc96d9d5bcaa398feb966e8463a13278f5
ybattist <yannis.battiston@capgemini.fr>
2020-10-11 23:09:37 +0200

    ajout application connexion back

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'StatsDeFootFront';
+
+  data = [{name}];
+
+  constructor(private dataService: DataService){}
+
+  ngOnInit(){
+    this.dataService.getAll().subscribe((response) => {
+      console.log(response)
+      this.data = response;
+    })
+  }
 }
